92747e3a0886dfe7feba824e44dcd15b2f2ba014
wickhill <wickhill@gmail.com>
2024-03-09 13:52:06 -0500

    added more info to seed database

--- models/citizen.js
+++ models/citizen.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const citizenSchema = new mongoose.Schema({
     name: {type: String, required: true},
     nationality: { type: String, default: "please-indicate-nationality"},
-    is_spy: {type: Boolean},
     hairColor: String,
     eyeColor: String,
+    is_spy: {type: Boolean},
     skills: [String],
     missions_completed: { type: String, default: "0" },
     charges: { type: String, default: "disloyalty" },
     testimonial: { type: String, default: "unverified-testimonial" },
+    requiresInvestigation: { type: Boolean },
     citizenProfileImage: { type: String, default: "defaultImgPath.jpg" },
-    requiresInvestigation: { type: Boolean }
 
   });
 
