822c3cd1b0ed942fe9d1c01e91180ef5d8b230d4
wickhill <wickhill@gmail.com>
2024-03-15 19:25:28 -0400

    deploying to Heroku

--- server.js
+++ server.js
@@ -43,12 +43,18 @@ const app = express();
 /* refresh configuration
 --------------------------------------------------------------- */
 
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
+// Detect if running in a dev environment
+if (process.env.ON_HEROKU === "false") {
+  // Configure the app to refresh the browser when nodemon restarts
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.server.once("connection", () => {
+    // wait for nodemon to fully restart before refreshing the page
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+  app.use(connectLiveReload());
+}
 
 /*
 --------------------------------------------------------------- */
